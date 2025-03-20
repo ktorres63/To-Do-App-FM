1c5730814dd5a6c26bd5ca563fbe5b0bbb3b61d5
ktorres63 <ktorresaro@unsa.edu.pe>
2025-03-20 00:07:19 -0500

    Add 4 backgrounds

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 import "./styles.css";
-console.log("TODO app")
+import sunIcon from "./images/icon-sun.svg"
+import moonIcon from "./images/icon-moon.svg"
+import lightMobileBg from "./images/bg-mobile-light.jpg"
+import lightDesktopeBg from "./images/bg-desktop-light.jpg"
+import darkMobileBg from "./images/bg-mobile-dark.jpg"
+import darkDesktopBg from "./images/bg-desktop-dark.jpg"
+
+
+
+
+
+const toogleDarkMode = document.getElementById("toggleTheme");
+const body = document.body;
+const icon = document.getElementById("darkModeIcon")
+const background = document.getElementsByClassName("background");
+
+toogleDarkMode.addEventListener("click", () =>{
+  body.classList.toggle("dark-mode")
+
+  if(body.classList.contains("dark-mode")){
+    icon.src= sunIcon;
+
+  }
+  else{
+    icon.src= moonIcon;
+
+  }
+
+
+
+})
