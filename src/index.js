103796c3bf27c49261f50536a326620a261d3543
ktorres63 <ktorresaro@unsa.edu.pe>
2025-03-20 23:27:03 -0500

    Add storage to darkmode

--- src/index.js
+++ src/index.js
@@ -1,32 +1,56 @@
 import "./styles.css";
-import sunIcon from "./images/icon-sun.svg"
-import moonIcon from "./images/icon-moon.svg"
-import lightMobileBg from "./images/bg-mobile-light.jpg"
-import lightDesktopeBg from "./images/bg-desktop-light.jpg"
-import darkMobileBg from "./images/bg-mobile-dark.jpg"
-import darkDesktopBg from "./images/bg-desktop-dark.jpg"
-
-
-
-
+import sunIcon from "./images/icon-sun.svg";
+import moonIcon from "./images/icon-moon.svg";
+import crossIcon from "./images/icon-cross.svg"
 
 const toogleDarkMode = document.getElementById("toggleTheme");
 const body = document.body;
-const icon = document.getElementById("darkModeIcon")
-const background = document.getElementsByClassName("background");
+const icon = document.getElementById("darkModeIcon");
 
-toogleDarkMode.addEventListener("click", () =>{
-  body.classList.toggle("dark-mode")
+if(localStorage.getItem("dark-mode") == "enabled"){
+  body.classList.add("dark-mode");
+  icon.src = sunIcon;
+}
 
-  if(body.classList.contains("dark-mode")){
-    icon.src= sunIcon;
 
+toogleDarkMode.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("dark-mode", "enabled")
+    icon.src = sunIcon;
+  } else {
+    localStorage.setItem("dark-mode", "disabled")
+    icon.src = moonIcon;
   }
-  else{
-    icon.src= moonIcon;
+});
 
-  }
+document
+  .querySelector(".todo-input")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
+    const input = document.getElementById("newTodo");
+    const todoText = input.value;
 
+    if (todoText == "") return;
 
-})
+    const li = document.createElement("li");
+    li.classList.add("todo-item")
+    li.innerHTML = ` 
+    <div class="li-text">
+      <span class="round"></span>
+      <p>${todoText}</p>
+    </div>
+    <button class="delete-btn">
+      <img src=${crossIcon}>
+    </button> `;
+
+    document.getElementById("todoList").appendChild(li);
+
+    input.value = "";
+
+    li.querySelector(".delete-btn").addEventListener("click", function (){
+      li.remove();
+    });
+  });
