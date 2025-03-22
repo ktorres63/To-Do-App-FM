4192df962f098492d898770999bc2b7fc8a4ab22
ktorres63 <ktorresaro@unsa.edu.pe>
2025-03-22 00:14:55 -0500

    Add filters

--- src/index.js
+++ src/index.js
@@ -7,9 +7,17 @@ const toogleDarkMode = document.getElementById("toggleTheme");
 const body = document.body;
 const icon = document.getElementById("darkModeIcon");
 const todoList = document.getElementById("todoList");
+const clearCompleted = document.getElementById("clearCompleted");
+
+const filterButtons = {
+  all: document.querySelectorAll(".btn-All"),
+  active: document.querySelectorAll(".btn-active"),
+  completed: document.querySelectorAll(".btn-completed"),
+};
+
 //Load all todos from localStorage
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
-
+let currentFilter = "all";
 
 // Get Dark Mode in LocalStorage
 if (localStorage.getItem("dark-mode") == "enabled") {
@@ -60,42 +68,71 @@ function saveAndRender() {
 function renderTodos() {
   todoList.innerHTML = "";
 
-  todos.forEach((todo, index) => {
+  todos.filter((todo) =>{
+    if(currentFilter == "active") return !todo.completed;
+    if(currentFilter == "completed") return todo.completed;
+    return true;
+  })
+  .forEach((todo, index) => {
     const li = document.createElement("li");
     li.classList.add("todo-item");
     if (todo.completed) li.classList.add("completed");
     li.innerHTML = ` 
     <div class="li-text">
-      <span class="round ${todo.completed ? "checked": ""}" data-index="${index}"></span>
+      <span class="round ${
+        todo.completed ? "checked" : ""
+      }" data-index="${index}"></span>
       <p>${todo.text}</p>
     </div>
-    <button class="delete-btn data-index="${index}">
+    <button class="delete-btn" data-index="${index}">
       <img src=${crossIcon}>
     </button> `;
 
-    todoList.appendChild(li)
+    todoList.appendChild(li);
   });
 }
 
 // mark as completed
 
-todoList.addEventListener("click", (e) =>{
-  if(e.target.classList.contains("round")){
+todoList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("round")) {
     const index = e.target.dataset.index;
     todos[index].completed = !todos[index].completed;
     saveAndRender();
-    console.log(`completed: ${e.target.dataset.index}`);
   }
-})
+});
 
 // delete todo
 
-todoList.addEventListener("click", (e) =>{
-  if(e.target.closest(".delete-btn")){
+todoList.addEventListener("click", (e) => {
+  if (e.target.closest(".delete-btn")) {
     const index = e.target.closest(".delete-btn").dataset.index;
-    todos.splice(index,1);
+    todos.splice(index, 1);
     saveAndRender();
   }
-})
+});
+
+function setFilter(filter) {
+  currentFilter = filter;
+
+  document.querySelectorAll(".buttons-footer a, .buttons a").forEach((btn) => {
+    btn.classList.remove("active");
+  });
+
+  filterButtons[filter].forEach((btn) => btn.classList.add("active"));
+
+  renderTodos();
+}
+
+Object.keys(filterButtons).forEach((filter) => {
+  filterButtons[filter].forEach((btn) => {
+    btn.addEventListener("click", () => setFilter(filter));
+  });
+});
 
 renderTodos();
+
+clearCompleted.addEventListener("click", () => {
+  todos = todos.filter(todo => !todo.completed);
+  saveAndRender();
+});
